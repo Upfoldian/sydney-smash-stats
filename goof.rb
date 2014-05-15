1dc60d9bc8989aa1074af247d557433812e5f760
root <root@localhost>
2014-05-15 12:39:58 +0000

    we internet now

--- goof.rb
+++ goof.rb
@@ -1,54 +1,58 @@
-require 'sinatra'
-require './lib/TioParse.rb'
-
-orderedBrackets = TioParse.order_brackets *TioParse.tio_files
+require 'sinatra/base'
+require './lib/tioParse.rb'
 
 #set :bind, '0.0.0.0'
 #set :port, 80
-get '/' do
-	erb :index, :locals => {:events => TioParse.get_singles(*orderedBrackets)}
-end
-
-get '/*/' do
-	searchTitle = params[:splat].first
-	if !TioParse.get_singles(*orderedBrackets).include? searchTitle.downcase
-		redirect to('/') 
-	end
-	test = TioParse::BracketGroup.new(orderedBrackets, searchTitle)
+class SydneySmashStats < Sinatra::Base
 	
-	players = test.eloHash.values.sort_by{|x| x.elo}.reverse
-	brackets = orderedBrackets.map{|x| x.split('/').last}.map{|x| x[0..-5]}
+	orderedBrackets = TioParse.order_brackets *TioParse.tio_files
+	
+	get '/' do
+		erb :index, :locals => {:events => TioParse.get_singles(*orderedBrackets)}
+	end
 
-	erb :events, :locals => {:players => players, 
-							 :brackets => brackets,
-							 :bracketTitle => searchTitle}
-end
+	get '/*/' do
+		searchTitle = params[:splat].first
+		if !TioParse.get_singles(*orderedBrackets).include? searchTitle.downcase
+			redirect to('/') 
+		end
+		test = TioParse::BracketGroup.new(orderedBrackets, searchTitle)
+		
+		players = test.eloHash.values.sort_by{|x| x.elo}.reverse
+		brackets = orderedBrackets.map{|x| x.split('/').last}.map{|x| x[0..-5]}
 
-#this needs to be tied with some unique bracket grouping ID or some shit
-get '/*/player=*' do
-	event = params[:splat].first
-	player = params[:splat].last
+		erb :events, :locals => {:players => players, 
+								 :brackets => brackets,
+								 :bracketTitle => searchTitle}
+	end
 
-	test = TioParse::BracketGroup.new(orderedBrackets, event)
-	if test.eloHash.has_key? player
-		erb :player, :locals => {:player => test.eloHash[player]}
-	else 
+	#this needs to be tied with some unique bracket grouping ID or some shit
+	get '/*/player=*' do
+		event = params[:splat].first
+		player = params[:splat].last
+
+		test = TioParse::BracketGroup.new(orderedBrackets, event)
+		if test.eloHash.has_key? player
+			erb :player, :locals => {:player => test.eloHash[player]}
+		else 
+			redirect to('/404')
+		end
+	end
+
+	get '/about' do
+		erb :about
+	end
+
+	get '/contact' do
+		erb :contact
+	end
+
+	get '/404' do
+		erb :not_found
+	end
+
+	not_found do
 		redirect to('/404')
 	end
-end
 
-get '/about' do
-	erb :about
-end
-
-get '/contact' do
-	erb :contact
-end
-
-get '/404' do
-	erb :not_found
-end
-
-not_found do
-	redirect to('/404')
 end
