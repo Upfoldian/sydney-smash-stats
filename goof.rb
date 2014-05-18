9e271bc56d99f39665f003deac81bd3a7f7e52d2
Thomas Upfold <summonerau@gmail.com>
2014-05-18 13:17:35 +1000

    moved back to classic stype sinatra

--- goof.rb
+++ goof.rb
@@ -1,72 +1,70 @@
-require 'sinatra/base'
+require 'sinatra'
 #require 'sinatra'
 require './lib/tioParse.rb'
 
 #set :bind, '0.0.0.0'
 #set :port, 80
 
-class SydneySmashStats < Sinatra::Base
-	orderedBrackets = TioParse.order_brackets *TioParse.tio_files
-	
-	get '/' do
-		erb :index, :locals => {:events => TioParse.get_singles(*orderedBrackets)}
+orderedBrackets = TioParse.order_brackets *TioParse.tio_files
+get '/' do
+	erb :index, :locals => {:events => TioParse.get_singles(*orderedBrackets)}
+end
+
+get '/*/' do
+	searchTitle = params[:splat].first
+	if !TioParse.get_singles(*orderedBrackets).include? searchTitle.downcase
+		redirect to('/') 
 	end
+	test = TioParse::BracketGroup.new(orderedBrackets, searchTitle)
 
-	get '/*/' do
-		searchTitle = params[:splat].first
-		if !TioParse.get_singles(*orderedBrackets).include? searchTitle.downcase
-			redirect to('/') 
-		end
-		test = TioParse::BracketGroup.new(orderedBrackets, searchTitle)
+	players = test.eloHash.values.sort_by{|x| x.elo}.reverse
 
-		players = test.eloHash.values.sort_by{|x| x.elo}.reverse
+	playerData = [["Player", "Elo"]]
+	players.each do |x|
+	 	next if x == nil
+	 	playerData << [x.name, x.elo]
+	end 
 
-		playerData = [["Player", "Elo"]]
-		players.each do |x|
-		 	next if x == nil
-		 	playerData << [x.name, x.elo]
-		end 
-
-		slices = players.each_slice((players.size/3.0).ceil).to_a
-		while slices.last.length < slices.first.length
-			slices.last << nil
-		end
-		players = slices.transpose.flatten
-
-		brackets = orderedBrackets.map{|x| x.split('/').last}.map{|x| x[0..-5]}
-
-		erb :events, :locals => {:players => players, 
-								 :brackets => brackets,
-								 :playerData => playerData,
-								 :bracketTitle => searchTitle}
+	slices = players.each_slice((players.size/3.0).ceil).to_a
+	while slices.last.length < slices.first.length
+		slices.last << nil
 	end
+	players = slices.transpose.flatten
 
-	#this needs to be tied with some unique bracket grouping ID or some shit
-	get '/*/player=*' do
-		event = params[:splat].first
-		player = params[:splat].last
+	brackets = orderedBrackets.map{|x| x.split('/').last}.map{|x| x[0..-5]}
 
-		test = TioParse::BracketGroup.new(orderedBrackets, event)
-		if test.eloHash.has_key? player
-			erb :player, :locals => {:player => test.eloHash[player]}
-		else 
-			redirect to('/404')
-		end
-	end
+	erb :events, :locals => {:players => players, 
+							 :brackets => brackets,
+							 :playerData => playerData,
+							 :bracketTitle => searchTitle}
+end
 
-	get '/about' do
-		erb :about
-	end
+#this needs to be tied with some unique bracket grouping ID or some shit
+get '/*/player=*' do
+	event = params[:splat].first
+	player = params[:splat].last
 
-	get '/contact' do
-		erb :contact
-	end
-
-	get '/404' do
-		erb :not_found
-	end
-
-	not_found do
+	test = TioParse::BracketGroup.new(orderedBrackets, event)
+	if test.eloHash.has_key? player
+		erb :player, :locals => {:player => test.eloHash[player]}
+	else 
 		redirect to('/404')
 	end
 end
+
+get '/about' do
+	erb :about
+end
+
+get '/contact' do
+	erb :contact
+end
+
+get '/404' do
+	erb :not_found
+end
+
+not_found do
+	redirect to('/404')
+end
+
